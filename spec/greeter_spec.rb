01b3190d66f5fa8e4e423d7d2078efc89a4ef103
Scott Denson <Scott.Denson@gmail.com>
2013-01-29 19:08:58 -0600

    Added elevator switching direction test.

--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -1,12 +1,18 @@
 class Elevator
-	attr_accessor :current_floor
+	attr_accessor :current_floor, :calling_floor
 
 	def initialize(floor)
 		@current_floor = floor
 	end
 
 	def call(floor)
-		@current_floor = floor
+		@calling_floor = floor
+	end
+
+	def tick()
+		if @calling_floor != @current_floor
+			@current_floor += (@current_floor > @calling_floor)?-1:1;
+		end
 	end
 
 end  
@@ -18,8 +24,25 @@ describe "Elevator" do
 			@elevator = Elevator.new(1)
 		end
 	  it "responds to calls" do
-	  	@elevator.call(3)
-	  	@elevator.current_floor.should be 3
+	  	@elevator.call(2)
+	  	@elevator.tick()
+	  	@elevator.current_floor.should be 2
 	  end  
+
+	  it "takes a tic to move" do
+	  	@elevator.call(3)
+	  	@elevator.tick()
+	  	@elevator.current_floor.should be 2
+	  	@elevator.tick()
+	  	@elevator.current_floor.should be 3
+	  end
+
+	  it "queues floors to visit" do
+	  	@elevator.call(3)
+	  	@elevator.tick
+	  	@elevator.call 1
+	  	@elevator.tick
+	  	@elevator.current_floor.should be 3 #we think
+	  end
   end 
 end  
