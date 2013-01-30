dc029056184c557cbc93b099e7a255212f7eb6f3
Scott Johnson <7.scott.j@gmail.com>
2013-01-29 19:23:37 -0600

    change calling floor to queue system

--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -1,20 +1,21 @@
 class Elevator
-	attr_accessor :current_floor, :calling_floor
+  attr_accessor :current_floor
 
-	def initialize(floor)
-		@current_floor = floor
-	end
+  def initialize(floor)
+  	@queue = []
+  	@current_floor = floor
+  end
 
-	def call(floor)
-		@calling_floor = floor
-	end
+  def call(floor)
+  	@queue << floor unless @queue.member? floor 
+  end
 
-	def tick()
-		if @calling_floor != @current_floor
-			@current_floor += (@current_floor > @calling_floor)?-1:1;
-		end
-	end
-
+  def tick()
+    return if @queue.empty?
+  	if @queue.first != @current_floor
+  		@current_floor += (@current_floor > @queue.first)?-1:1;
+  	end
+  end
 end  
 
 
@@ -23,7 +24,8 @@ describe "Elevator" do
 		before do
 			@elevator = Elevator.new(1)
 		end
-	  it "responds to calls" do
+	  
+    it "responds to calls" do
 	  	@elevator.call(2)
 	  	@elevator.tick()
 	  	@elevator.current_floor.should be 2
@@ -44,5 +46,14 @@ describe "Elevator" do
 	  	@elevator.tick
 	  	@elevator.current_floor.should be 3 #we think
 	  end
+
+    it "should remove the current floor from the queue" do
+      @elevator.call(2)
+      @elevator.tick()
+      @elevator.call(3)
+      @elevator.tick()
+      @elevator.current_floor.should be 3 
+    end
+
   end 
 end  
